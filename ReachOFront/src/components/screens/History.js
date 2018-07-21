43f4b6633355b712a23f4bf130f012792ae28259
Steffany Brown <steffannbrown@gmail.com>
2018-07-20 21:39:21 -0700

    added function to make colors array for chart creation

--- ReachOFront/src/components/screens/History.js
+++ ReachOFront/src/components/screens/History.js
@@ -44,7 +44,17 @@ class History extends React.Component {
   makeWeekApiCall(uid) {
     api.getWeekLogData('0741709239').then((data) => {
       this.makeColorArray(data)
-      this.setState({chartData: data})
+
+      const dataLength = data.length
+      let dataArray = []
+
+      for(let i=0; i < dataLength; i++){
+        let yVal = data[i].primary_mood
+        let yObject = {y: yVal}
+        dataArray.push(yObject)
+      }
+
+      this.setState({chartData: dataArray})
       console.log('oOOOOOooOoOoooooooOooooo')
       console.log(this.state)
     })
@@ -60,16 +70,85 @@ class History extends React.Component {
 
   makeColorArray(data) {
 
-    console.log(data.length)
-    const logNum = data.length
-    let array = []
+    const dataLength = data.length;
+    let colorArray = []
 
-    for(let i=0; i < logNum; i++){
-      array.push('GREEN')
+    for(let i=0; i < dataLength; i++){
+      let primary = data[i].primary_mood;
+      let intensity = data[i].intensity;
+      let color = this.checkColor(primary, intensity);
+
+      colorArray.push(color)
+      this.setState({colorArray: colorArray})
     }
-    console.log(array)
-    this.setState({colorArray: array})
 
+  }
+
+  checkColor(primary, intensity) {
+    if (primary > -1) {
+      if (intensity > -1 && intensity < 3) {
+        return '#FFFFD9'
+      }
+      else if (intensity == 3 || intensity == 4) {
+        return '#FFFFB2'
+      }
+      else if (intensity == 5 || intensity == 6) {
+        return '#FFFF8C'
+      }
+      else if (intensity == 7 || intensity == 8) {
+        return '#FFFF66'
+      }
+      else if (intensity == 9 || intensity == 10) {
+        return '#FFFF00'
+      }
+      else if (intensity < 0 && intensity > -3) {
+        return '#E7FFE7'
+      }
+      else if (intensity == -3 || intensity == -4) {
+        return '#C1FFC1'
+      }
+      else if (intensity == -5 || intensity == -6) {
+        return '#90EE90'
+      }
+      else if (intensity == -7 || intensity == -8) {
+        return '#5BC85B'
+      }
+      else if (intensity == -9 || intensity == -10) {
+        return '#31A231'
+      }
+    }
+    else if (primary < 0) {
+      if (intensity > -1 && intensity < 3) {
+        return '#FF7373'
+      }
+      else if (intensity == 3 || intensity == 4) {
+        return '#FF4C4C'
+      }
+      else if (intensity == 5 || intensity == 6) {
+        return '#FF2626'
+      }
+      else if (intensity == 7 || intensity == 8) {
+        return '#D90000'
+      }
+      else if (intensity == 9 || intensity == 10) {
+        return '#B20000'
+      }
+      else if (intensity < 0 && intensity > -3) {
+        return '#EBF5FF'
+      }
+      else if (intensity == -3 || intensity == -4) {
+        return '#C4E3FF'
+      }
+      else if (intensity == -5 || intensity == -6) {
+        return '#9ED1FF'
+      }
+      else if (intensity == -7 || intensity == -8) {
+        return '#78BFFF'
+      }
+      else if (intensity == -9 || intensity == -10) {
+        return '#4593D9'
+      }
+    }
   }
 
 
