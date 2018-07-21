91aec64d0e92c6d9135ea2003366b838f97bede5
Steffany Brown <steffannbrown@gmail.com>
2018-07-21 15:59:17 -0700

    cleaned up constants for moodchart

--- ReachOFront/src/components/screens/History.js
+++ ReachOFront/src/components/screens/History.js
@@ -69,7 +69,6 @@ class History extends React.Component {
   }
 
   makeColorArray(data) {
-
     const dataLength = data.length;
     let colorArray = []
 
@@ -78,88 +77,95 @@ class History extends React.Component {
       let intensity = data[i].intensity;
       let color = this.checkColor(primary, intensity);
 
-      colorArray.push(color)
-      this.setState({colorArray: colorArray})
+      colorArray.push(color);
+      this.setState({colorArray: colorArray});
     }
-
   }
 
   checkColor(primary, intensity) {
     if (primary > -1) {
       if (intensity > -1 && intensity < 3) {
-        return '#FFFFD9'
+        return processColor('#FFFFD9')
       }
       else if (intensity == 3 || intensity == 4) {
-        return '#FFFFB2'
+        return processColor('#FFFFB2')
       }
       else if (intensity == 5 || intensity == 6) {
-        return '#FFFF8C'
+        return processColor('#FFFF8C')
       }
       else if (intensity == 7 || intensity == 8) {
-        return '#FFFF66'
+        return processColor('#FFFF66')
       }
       else if (intensity == 9 || intensity == 10) {
-        return '#FFFF00'
+        return processColor('#FFFF00')
       }
       else if (intensity < 0 && intensity > -3) {
-        return '#E7FFE7'
+        return processColor('#E7FFE7')
       }
       else if (intensity == -3 || intensity == -4) {
-        return '#C1FFC1'
+        return processColor('#C1FFC1')
       }
       else if (intensity == -5 || intensity == -6) {
-        return '#90EE90'
+        return processColor('#90EE90')
       }
       else if (intensity == -7 || intensity == -8) {
-        return '#5BC85B'
+        return processColor('#5BC85B')
       }
       else if (intensity == -9 || intensity == -10) {
-        return '#31A231'
+        return processColor('#31A231')
       }
     }
     else if (primary < 0) {
       if (intensity > -1 && intensity < 3) {
-        return '#FF7373'
+        return processColor('#FF7373')
       }
       else if (intensity == 3 || intensity == 4) {
-        return '#FF4C4C'
+        return processColor('#FF4C4C')
       }
       else if (intensity == 5 || intensity == 6) {
-        return '#FF2626'
+        return processColor('#FF2626')
       }
       else if (intensity == 7 || intensity == 8) {
-        return '#D90000'
+        return processColor('#D90000')
       }
       else if (intensity == 9 || intensity == 10) {
-        return '#B20000'
+        return processColor('#B20000')
       }
       else if (intensity < 0 && intensity > -3) {
-        return '#EBF5FF'
+        return processColor('#EBF5FF')
       }
       else if (intensity == -3 || intensity == -4) {
-        return '#C4E3FF'
+        return processColor('#C4E3FF')
       }
       else if (intensity == -5 || intensity == -6) {
-        return '#9ED1FF'
+        return processColor('#9ED1FF')
       }
       else if (intensity == -7 || intensity == -8) {
-        return '#78BFFF'
+        return processColor('#78BFFF')
       }
       else if (intensity == -9 || intensity == -10) {
-        return '#4593D9'
+        return processColor('#4593D9')
       }
     }
   }
 
 
 
- render() {
-   const vals = [{y: -224.1}, {y: 238.5}, {y: 1280.1}]
+render() {
+   const moodVals = [{y: -10}, {y: -10}, {y: -10}, {y: -10}, {y: -10},
+     {y: -10}, {y: -10}, {y: -10}, {y: -10}, {y: -10},
+     {y: -10}, {y: -10}, {y: -10}, {y: -10}, {y: -10}, {y: -10},
+     {y: 5}, {y: -2}, {y: -5}, {y: 5}, {y: 5}]
+
+    const colorVals = [-12217383, -12217383, -12217383, -12217383, -12217383, -12217383,
+      -12217383, -12217383, -12217383, -12217383, -12217383, -12217383, -12217383,
+      -12217383, -12217383, -12217383, -39, -35981, -2555904, -10762149, -10762149]
 
    return (
 
      <View style={{flex: 1}}>
-      <MoodChart logValues={ this.state.chartData } colors= { this.state.colorArray}/>
+      <MoodChart logValues={moodVals} colors={colorVals}/>
+
      </View>
 
    );
