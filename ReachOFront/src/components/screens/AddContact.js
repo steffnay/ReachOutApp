3dd8e7cae9b362804d6f235d14093960569a7284
Steffany Brown <steffannbrown@gmail.com>
2018-07-24 13:22:44 -0700

    fixed logic for phone number style and added consistent styling

--- ReachOFront/src/components/screens/AddContact.js
+++ ReachOFront/src/components/screens/AddContact.js
@@ -29,10 +29,18 @@ class AddContact extends Component {
       this.setState({last_name: text,})
     }
     else if(field == 'phone') {
-      this.setState({phone: text,})
+      if (text.length == 10) {
+        const area = text.substr(0,3);
+        const first = text.substr(3,3);
+        const last = text.substr(6,4);
+        text = `${area}-${first}-${last}`}
+      else if (text.length > 10) {
+        text = text.substr(0, 10)
+      }
+      this.setState({phone: text});
     }
     else if(field == 'email') {
-      this.setState({email: text})
+      this.setState({email: text});
     }
   }
 
@@ -45,18 +53,16 @@ class AddContact extends Component {
     const uid = currentUser._user.uid
 
     let collection = {}
-    collection.first_name = this.state.first_name
-    collection.last_name = this.state.last_name
-    collection.phone = this.state.phone
-    collection.email = this.state.email
-    collection.uid = uid
+    collection.first_name = this.state.first_name;
+    collection.last_name = this.state.last_name;
+    collection.phone = this.state.phone;
+    collection.email = this.state.email;
+    collection.id = uid;
 
-    console.log(collection)
-
-    api.createContact(collection).then(() => {
+    api.createContact(collection).then((response) => {
       const params = this.props.navigation.state;
       params.params.updateList();
-      this.props.navigation.navigate('Contacts')
+      this.props.navigation.navigate('Contacts');
     })
   }
 
@@ -75,9 +81,12 @@ class AddContact extends Component {
             />
         <TextInput placeholder= {'Phone'}
             style = {styles.input}
+            keyboardType = {'phone-pad'}
+            maxLength={10}
             onChangeText={(text) => this.updateValue(text,'phone')}
             />
         <TextInput placeholder= {"Email"}
+          keyboardType={'email-address'}
           style = {styles.input}
           onChangeText={(text) => this.updateValue(text,'email')}
           />
@@ -96,12 +105,12 @@ class AddContact extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#fce0c7',
     flex: 1,
     justifyContent: 'center',
   },
   button: {
-    backgroundColor: 'skyblue',
+    backgroundColor: '#4a4a4a',
     height: 40,
     color: 'white',
     justifyContent: 'center',
